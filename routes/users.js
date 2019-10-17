5cd7154ea46e8a59f8731fb416c221329d8d6a7a
Vong3432 <vongnyuksoon2000@gmail.com>
2019-10-17 21:44:55 +0800

    Remove unncessary files(Like mongo stuff)

--- routes/users.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const bcrypt = require('bcrypt')
-const jwt = require('jwt')
-
-const User = require('../models/User')
--- server.js
+++ server.js
@@ -1,23 +1,3 @@
-/*const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
-
-// to get data from res.body
-app.use(express.json())
-
-const mongoURL = "mongodb+srv://vong3432:34325417@cluster0-eqbsb.mongodb.net/test?retryWrites=true&w=majority";
-
-// connect to mongoDB
-mongoose
-    .connect(mongoURL, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
-    .then( () => console.log("MongoDB Connected..."))
-    .catch( err => console.log(err) );
-
-
-const port = process.env.port || 5000
-
-app.listen(port, () => console.log(`Listen on port ${port}`))
-*/
 const express = require('express')
 const app = express()
 
